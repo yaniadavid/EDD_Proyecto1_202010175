444bc0995f892a49a0f1e15121b0484e5108a6bc
Yania Eszter Dávid <3855921830108@ingenieria.usac.edu.gt>
2022-12-16 11:10:23 -0600

    register

--- js/main.js
+++ js/main.js
@@ -232,51 +232,198 @@ class queueBlock{
 }
 
 
+//------------------------------ ARTISTAS + CANCIONES -------------------
+class NodoCabeza{
+    constructor(name, age, country){
+        this.name = name;
+        this.age = age;
+        this.country = country;
+        this.next = null;
+        this.down = null;
+    }
+}
+
+class NodoValor{
+    constructor(artist, name, duration, gender){
+        this.artist = artist;
+        this.name = name;
+        this.duration = duration;
+        this.gender = gender;
+        this.next = null;
+    }
+}
+
+
+class Listadelistas{
+    constructor(){
+        this.head = null;
+    }
+    //metodos de insertar
+    InsertarCabeceras(name, age, country){
+        var temporal = new NodoCabeza(name, age, country);
+        temporal.next = this.head;
+        this.head = temporal;
+    }
+
+    InsertarValores(artist, name, duration, gender){
+        var temporalcabeza = this.head;
+        //recorrer toda la lista de cabezas 
+        while(temporalcabeza != null){
+            if(temporalcabeza.name == artist){
+                var nuevacancion = new NodoValor(artist, name, duration, gender);
+                var iniciocanciones = temporalcabeza.down
+                temporalcabeza.down = nuevacancion
+                nuevacancion.next = iniciocanciones
+                break
+            }
+            temporalcabeza= temporalcabeza.next
+        }
+        if(temporalcabeza == null){
+            console.log("No se encontro el cabecera en la lista "+ artist)
+        }
+
+    }
+
+    mostrarCabeceras(){
+        var temporal = this.head;
+        console.log("*********** Cabeceras *********");
+        while (temporal != null){
+            console.log(temporal.name);
+            temporal = temporal.next;
+        }
+    }
+
+    MostrarValores(artist){
+        var temporal = this.head
+        while (temporal != null){
+            if(temporal.name == artist){
+                console.log("*********** Cabecera "+ artist +" *********")        
+                var temporalcanciones = temporal.down
+                while(temporalcanciones != null){
+                    console.log(temporalcanciones.name)
+                    temporalcanciones = temporalcanciones.next
+                }
+                return
+            }
+            temporal = temporal.next
+        }
+        if(temporal == null){
+            console.log("No se pudo encontrar el cabeza solicitado "+_value)
+        }
+    }
+}
+
+//----------------------------VARIABLES GLOBALES------------------------
+var Users = new listUsers();
+var Album = new Listadelistas();
+
+
+
+
+
+//-----------------Admin Auxiliar---------------------------------------
+Users.addUser(2654568452521, "Oscar Armin", "EDD", "123", "+502 (123) 123-4567", true)
+
+
+Users.printUsers();
 
 // --------------------------- LOGIN --------------------------------
-document.getElementById("btn_login").onclick=function(){
+document.getElementById('btn_login').onclick=function(){
     console.log("Intento de Inicio de Sesión");
-    let user = document.getElementById("userLogin").value;
-    let password = document.getElementById("passwordLogin").value;
-    var usuarioEntrada = listUsers.findUserLogin(user,password);
-    var check = document.getElementById("checkAdm").check;
+    var user = document.getElementById('userLogin').value;
+    //console.log(user)
+    var password = document.getElementById('passwordLogin').value;
+    //console.log(password)
+    var usuarioEntrada = Users.findUserLogin(user,password);
+    //console.log(usuarioEntrada);
+    //console.log(check);
+    //console.log(usuarioEntrada);
     if (usuarioEntrada != null){
-       if(check = true && usuarioEntrada.admin == true){
+       if(document.getElementById('checkAdm').checked == true && usuarioEntrada!=null){
             console.log("Intento de Inicio de Sesión Exitoso");
-            alert("Ingreso como Administrador: " + usuarioEntrada.username);
-            document.getElementById("NavBar").style.display="none";
-            document.getElementById("NavBar2").style.display="block";
-            document.getElementById("NarBar3").style.display="none";
-            document.getElementById("Index").style.display="none";
-            document.getElementById("Login").style.display="none";
-            document.getElementById("Register").style.display="none";
-            document.getElementById("Admin").style.display="block";
-            document.getElementById("User").style.display="none";
+           // alert("Ingreso como Administrador: " + usuarioEntrada.username);
+            document.getElementById('NavBar1').style.display="none";
+            document.getElementById('NavBar2').style.display="block";
+            document.getElementById('NavBar3').style.display="none";
+            document.getElementById('Index').style.display="none";
+            document.getElementById('Login').style.display="none";
+            document.getElementById('Register').style.display="none";
+            document.getElementById('Admin').style.display="block";
+            document.getElementById('User').style.display="none";
+
+            console.log("Comprobación")
             
             //document.getElementById("NarBar3").style.display="none";
             //document.getElementById("Administracion").style.display="block";
         }else{
             alert("Ingreso de Usuario: " + usuarioEntrada.username);
-            document.getElementById("NavBar").style.display="none";
-            document.getElementById("NavBar2").style.display="none";
-            document.getElementById("NarBar3").style.display="block";
-            document.getElementById("Index").style.display="none";
-            document.getElementById("Login").style.display="none";
-            document.getElementById("Register").style.display="none";
-            document.getElementById("Admin").style.display="none";
-            document.getElementById("User").style.display="block";
+            document.getElementById('NavBar1').style.display="none";
+            document.getElementById('NavBar2').style.display="none";
+            document.getElementById('NavBar3').style.display="block";
+            document.getElementById('Index').style.display="none";
+            document.getElementById('Login').style.display="none";
+            document.getElementById('Register').style.display="none";
+            document.getElementById('Admin').style.display="none";
+            document.getElementById('User').style.display="block";
             //document.getElementById("Login").style.display="none";
             //document.getElementById("Index").style.display="none";
             //document.getElementById("Administracion").style.display="none";
             //document.getElementById("PaginaUsuario").style.display="block";
        }
     }else{
-        console.log("Intento de Inicio de Sesión Fallido");
+        
         alert("Usuario o contraseña incorrectos");
         document.getElementById("userLogin").value="";
         document.getElementById("passwordLogin").value="";
+        console.log("Intento de Inicio de Sesión Fallido");
     }
     //listaUsuarios.graficarUsuarios();
+}
+
+document.getElementById("btn_gotoregister").onclick=function(){
+    document.getElementById('NavBar1').style.display="block";
+    document.getElementById('NavBar2').style.display="none";
+    document.getElementById('NavBar3').style.display="none";
+    document.getElementById('Index').style.display="none";
+    document.getElementById('Login').style.display="none";
+    document.getElementById('Register').style.display="block";
+    document.getElementById('Admin').style.display="none";
+    document.getElementById('User').style.display="none";
+}
+
+document.getElementById("btn_cancelRegister").onclick=function(){
+    document.getElementById('NavBar1').style.display="block";
+    document.getElementById('NavBar2').style.display="none";
+    document.getElementById('NavBar3').style.display="none";
+    document.getElementById('Index').style.display="none";
+    document.getElementById('Login').style.display="block";
+    document.getElementById('Register').style.display="none";
+    document.getElementById('Admin').style.display="none";
+    document.getElementById('User').style.display="none";
+}
+
+document.getElementById("btn_register").onclick=function(){
+    var usernameR = document.getElementById('usernameR').value;
+    var fullnameR = document.getElementById('fullnameR').value;
+    var dpiR = document.getElementById('dpiR').value;
+    var phoneR = document.getElementById('phoneR').value;
+    var pswR = document.getElementById('pswR').value;
+
+    Users.addUser(dpiR, fullnameR, usernameR, pswR, phoneR, false);
+
+    alert('Usuario ' + usernameR + ' creado exitosamente.');
+     
+
+    document.getElementById('NavBar1').style.display="block";
+    document.getElementById('NavBar2').style.display="none";
+    document.getElementById('NavBar3').style.display="none";
+    document.getElementById('Index').style.display="none";
+    document.getElementById('Login').style.display="block";
+    document.getElementById('Register').style.display="none";
+    document.getElementById('Admin').style.display="none";
+    document.getElementById('User').style.display="none";
+
+    Users.printUsers();
 }
 
 function leerArchivoUsuario(e) {
@@ -302,3 +449,6 @@ function loadUsers(content){
    // listaUsuarios.graficarUsuarios();
     alert("Usuarios cargados"); 
 }
+
+
+
